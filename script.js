742225adfeab6c5959533494977ffe41978a85e9
Fraol Ayehu <phrolaye@gmail.com>
2023-07-18 07:53:31 -0500

    attack 1

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ $(document).ready(onReady);
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+
+ let fungusHP = 100;
+
+ let myAP = 100;
 
 function onReady() {
     
+     $('#attack-btn arcane-scepter').on('click', arcaneScepter);
+
+     function arcaneScepter() {
+        fungusHP - 14;
+        myAP - 12;
+     }
+
+
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
